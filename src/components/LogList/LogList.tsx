09cb9a4459e897f4cab66ed99510bc018920ee47
Andrew Stalick <andy@stalick.net>
2024-08-30 11:39:43 -0500

    splitting LogList so we can do better mocking and isolate data viewmodel creation

--- src/components/LogList/LogList.tsx
+++ src/components/LogList/LogList.tsx
@@ -1,8 +1,7 @@
 import './LogList.css';
 
-import Loader from '../Loader';
 import { LogDataContext } from '../../utils/Contexts';
-import LogListItem from '../LogListItem';
+import LogListView from './LogListView';
 import { TOTALROWS } from '../../utils/constants';
 import { useContext } from 'react';
 
@@ -10,27 +9,11 @@ const LogList: React.FC = () => {
   const logData = useContext(LogDataContext);
 
   return (
-    <div className="log-list">
-      <div className="list-header">
-        <div className="list-header list-header-time">Time</div>
-        <div className="list-header list-header-event">Event</div>
-      </div>
-      {logData.loading && (
-        <Loader
-          loaded={Object.keys(logData.logItems).length}
-          total={TOTALROWS}
-        />
-      )}
-      <div className="list-body">
-        {Object.keys(logData.logItems).map((key, index) => (
-          <LogListItem
-            key={`${index}-${key}`}
-            logItem={logData.logItems[key]}
-            rawTime={key}
-          />
-        ))}
-      </div>
-    </div>
+    <LogListView
+      loading={logData.loading}
+      logItems={logData.logItems}
+      totalRows={TOTALROWS}
+    ></LogListView>
   );
 };
 
---
+++ src/components/LogList/LogListView.tsx
@@ -0,0 +1,37 @@
+import './LogList.css';
+
+import Loader from '../Loader';
+import type { LogItem } from '../../hooks/useLogData';
+import LogListItem from '../LogListItem';
+
+interface LogListViewProps {
+  loading: boolean;
+  logItems: { [key: string]: LogItem };
+  totalRows: number;
+}
+
+const LogListView: React.FC<LogListViewProps> = (props) => {
+  const { loading, logItems, totalRows } = props;
+  return (
+    <div className="log-list">
+      <div className="list-header">
+        <div className="list-header list-header-time">Time</div>
+        <div className="list-header list-header-event">Event</div>
+      </div>
+      {loading && (
+        <Loader loaded={Object.keys(logItems).length} total={totalRows} />
+      )}
+      <div className="list-body">
+        {Object.keys(logItems).map((key, index) => (
+          <LogListItem
+            key={`${index}-${key}`}
+            logItem={logItems[key]}
+            rawTime={key}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LogListView;
